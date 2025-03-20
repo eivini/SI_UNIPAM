8ddd8d288ad60200713bf0ca44fc114b5c17a69b
Halphar <143683210+eivini@users.noreply.github.com>
2025-03-20 23:14:25 +0000

    Ex3 da atividade_pratica_01

---
+++ Desenvolvimento para web II/atividade_pratica_01/MarcusAlves_Lista1/Ex3/ex3/src/main/java/aplicativo/Principal.java
@@ -0,0 +1,48 @@
+package aplicativo;
+
+import dominio.Pessoa;
+import dominio.Vendedor;
+import dominio.Cliente;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula-jpa"); //Instancia o EntityManagerFactory com as configurações de persistencia
+		EntityManager em = emf.createEntityManager(); //Intancia o EntityManager
+		
+		Vendedor vendedor1 = new Vendedor("Marcus Vinícius", 27, 0001);
+		Cliente cliente1 = new Cliente("Rafael", 34, 0001);
+		
+		em.getTransaction().begin();// iniciar transação com banco de dados
+		
+		em.persist(vendedor1);
+		em.persist(cliente1);
+		
+		Query consultaV = em.createQuery("select vendedor from Vendedor vendedor"); //consulta em jpql
+		ArrayList<Vendedor> listaV = (ArrayList<Vendedor>) consultaV.getResultList();
+		
+		Query consultaC = em.createQuery("select cliente from Cliente cliente"); //consulta em jpql
+		ArrayList<Cliente> listaC = (ArrayList<Cliente>) consultaC.getResultList();
+		
+		em.getTransaction().commit();//confirmar as alterações realizadas
+		
+		emf.close();
+		em.close();
+		
+		for(Vendedor objV: listaV) {
+			System.out.println(objV);
+		}
+		
+		for(Cliente objC: listaC) {
+			System.out.println(objC);
+		}
+	}			
+}
